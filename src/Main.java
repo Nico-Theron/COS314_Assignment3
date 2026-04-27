462470982ae068052115a3351d52e44110bb4628
Nico-Theron <u23565722@tuks.co.za>
2026-04-27 21:46:25 +0200

    Add GenerateTree to create random tree candidates

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import tree.Node;
+import tree.TreeGenerator;
+
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Random random = new Random(1234);
+        TreeGenerator generator = new TreeGenerator(random);
+
+        Node tree = generator.generateRampedHalfAndHalf(3);
+
+        System.out.println("Generated tree:");
+        System.out.println(tree);
+
+        int[] exampleRow = {
+                3, // age
+                1, // menopause
+                4, // tumor_size
+                1, // inv_nodes
+                0, // node_caps
+                2, // deg_malig
+                1, // breast
+                2, // breast_quad
+                0  // irradiat
+        };
+
+        int prediction = tree.classify(exampleRow);
+        System.out.println("Prediction: " + prediction);
     }
 }
