c9afbe4a4812a454a36b7cef91b7d948ef15c311
Nico-Theron <u23565722@tuks.co.za>
2026-04-28 01:07:56 +0200

    Moved the Algorithm specifics out of Main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import data.DataLoader;
 import data.Dataset;
-import fitness.FitnessEvaluator;
-import tree.Node;
-import tree.TreeGenerator;
 import gp.Individual;
-import gp.Population;
+import gp.LogicalGPAlgorithm;
+
 import java.util.Random;
 
 public class Main {
@@ -12,23 +10,11 @@ public class Main {
         Dataset trainData = DataLoader.loadCSV("Breast_train.csv");
 
         Random random = new Random(1234);
-        TreeGenerator generator = new TreeGenerator(random);
-        FitnessEvaluator evaluator = new FitnessEvaluator();
 
-        Node tree = generator.generateRampedHalfAndHalf(3);
+        LogicalGPAlgorithm gp = new LogicalGPAlgorithm(random);
+        Individual best = gp.train(trainData);
 
-        double accuracy = evaluator.calculateAccuracy(tree, trainData);
-
-        System.out.println("Generated tree:");
-        System.out.println(tree);
-        System.out.println("Accuracy: " + (accuracy * 100) + "%");
-
-        Population population = new Population(200, generator, 3);
-        population.evaluate(evaluator, trainData);
-
-        Individual best = population.getBestIndividual();
-
-        System.out.println("Best individual in population:");
+        System.out.println("FINAL BEST TREE:");
         System.out.println(best);
     }
 }
